70dd35397e78251a812b6e37a0379b479d3bec89
AdityaDeolalikar <adityadeolalikar19@gmail.com>
2026-08-16 16:09:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TestimonialCard from "./components/TestimonialCard";
+
+function App() {
+  return (
+    <main className="min-h-screen w-full flex items-center justify-center bg-gradient-to-b from-gray-50 to-[#d2d6db] p-4">
+      <TestimonialCard />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,26 @@
+import profileImg from "../assets/img/profile-thumbnail.png";
+
+function TestimonialCard() {
+    return (
+        <div className="w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
+            <div className="flex items-center gap-4 self-stretch">
+                <div className="w-12 h-12">
+                    <img
+                        src={profileImg}
+                        alt="Sarah Dole"
+                        className="w-12 h-12 rounded-full object-cover"
+                    />
+                </div>
+                <div className="flex flex-col gap-px grow">
+                    <span className="font-semibold text-lg text-neutral-900">Sarah Dole</span>
+                    <span className="font-normal text-sm text-neutral-600">@sarahdole</span>
+                </div>
+            </div>
+            <span className="font-normal text-base text-neutral-600">
+                I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!
+            </span>
+        </div>
+    );
+}
+
+export default TestimonialCard;
